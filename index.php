783d75b06196550f62dc224d9c8e29f29235db9c
Andrew Rohne <ke8p@ke8p.us>
2015-06-03 22:21:26 -0400

    program setup page

--- index.php
+++ index.php
@@ -14,9 +14,9 @@ $dbh = new PDO('sqlite:db/fermpi.db');
 <title>FermPi Fermentation Controller</title>
 </head>
 <body>
-<p>Sitrep</p>
-<p>Fermentation Chamber Status</p>
-<p>Fermentation Chamber Setup</p>
-<p>Fermentation Program Setup</p>
+<p><a href="sitrep.php">Sitrep</a></p>
+<p><a href="chamber-status.php">Fermentation Chamber Status</a></p>
+<p><a href="chamber-setup.php">Fermentation Chamber Setup</a></p>
+<p><a href="program-setup.php">Fermentation Program Setup</a></p>
 </body>
 </html>
---
+++ program-setup.php
@@ -0,0 +1,28 @@
+<?php
+$db = new PDO('sqlite:db/fermpi.db');
+$fs = $db->prepare("SELECT profileName FROM fermSchedules;");
+$fermScheds = fs->fetchAll();
+print_r($fermScheds);
+
+?>
+<html>
+<head>
+function getProg(progName){
+	if(progName != ""){
+		if(window.XMLHttpRequest){
+			xmlhttp = new XMLHttpRequest();
+		}else{
+			xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		xmlhttp.onreadystatechange = function(){
+			if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+				document.getElementById("id").prop = xmlhttp.responseText;
+			}
+		}
+		xmlhttp.open("GET","getProgram.php?q="+progName,true);
+		xmlhttp.send();
+	}
+}
+</script>
+</head>
+<body>
