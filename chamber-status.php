e64f29aca462502cadfedf63f6e61410325a62d5
Andrew Rohne <ke8p@ke8p.us>
2015-07-06 22:03:11 -0400

    some status updates

--- chamber-status.php
+++ chamber-status.php
@@ -1,6 +1,40 @@
 <?php
 include("functions.php");
 
+$db = new PDO('sqlite:db/fermpi.db');
+$cs = $db->prepare("SELECT eventID, timeStamp FROM fermStatus");
+$cs->execute();
+$currStat = $cs->fetchAll();
+
+
+$profId = $currStat[0]["eventID"];
+
+$fs = $db->prepare("SELECT * FROM fermSchedules WHERE fermScheduleId=".$profId);
+$fs->execute();
+$fermScheds = $fs->fetchAll();
+
+$currTime = time();
+$dur = $currTime - $currStat[0]["timeStamp"];
+
+$durDay = $dur / 3600 / 24;
+$currSetTemp = 50;
+
+if($durDay<=$fermScheds[0]["primaryDays"]){
+  //echo "In primary";
+  $currSetTemp = $fermScheds[0]["primaryTemp"];
+}elseif($durDay<=($fermScheds[0]["primaryDays"]+$fermScheds[0]["diacetylRestDays"])){
+  echo "In diacetyl rest";
+  $currSetTemp = $fermScheds[0]["diacetylRestTemp"];
+}elseif($durDay<=($fermScheds[0]["primaryDays"]+$fermScheds[0]["diacetylRestDays"]+$fermScheds[0]["lagerDays"])){
+  echo "in Lager";
+  $currSetTemp = $fermScheds[0]["lagerTemp"];
+}else{
+  echo "past lager";
+  $currSetTemp = $fermScheds[0]["lagerTemp"];
+}
+
+$currTempFile = fopen("current_temperature", "r");
+$currTemp = fread($currTempFile,filesize("current_temperature"));
 
 ?>
 <html>
@@ -30,12 +64,16 @@ include("functions.php");
 
   </script>
 </head>
-<body onload="getStatus();">
+<body>
   <?php getHeader() ?>
   <div class="fsSpacer"></div>
   <div class="fermStatus" id="fermStatus">
+    <p>Currently running the <?php echo $fermScheds[0]["profileName"]; ?> program</p>
+    <p>Chamber is currently at <?php echo $currTemp; ?>&deg;F and set at <?php echo $currSetTemp; ?>&deg;F</p>
+
+
   <!-- start, current step, time remaining in step, time remaining in program -->
-  
+
   </div>
 </body>
 </html>
